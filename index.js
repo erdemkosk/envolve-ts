f4767fed6e3e694f25b472ddc37e1daa6a64964d
Erdem Köşk <erdemkosk@gmail.com>
2023-10-25 01:09:00 +0300

    Added chalk

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 const program = require('commander');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
 
 program
   .version('1.0.0')
   .description('Envolve CLI Tool');
 
 program
-  .command('c')
+  .command('create')
   .description('Create a new env file')
   .action(async () => {
     const answers = await inquirer.prompt([
       {
         type: 'input',
-        name: 'filename',
-        message: 'Enter the env name: ',
+        name: 'servicename',
+        message: chalk.green('Enter the service name: '),
       },
       {
         type: 'editor',
         name: 'content',
-        message: 'Enter the env content: ',
+        message: chalk.green('Enter the env content: '),
       },
     ]);
 
-    const { servicename, filename, content } = answers;
+    const { servicename, content } = answers;
 
     try {
-      await fs.promises.writeFile(`${filename}`, content, 'utf8');
-      console.log(`File "${filename}" created in the "${servicename}" directory.`);
+      const serviceFolderPath = path.join(__dirname, servicename);
+      if (!fs.existsSync(serviceFolderPath)) {
+        fs.mkdirSync(serviceFolderPath);
+      }
+
+      await fs.promises.writeFile(path.join(serviceFolderPath, '.env'), content, 'utf8');
+      console.log(`File .env created in the "${chalk.blue(servicename)}" directory.`);
     } catch (error) {
       console.error('An error occurred:', error);
     }
